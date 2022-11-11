57612892fe6c4d8f07119647d8c85ca6fe1f07b4
Дмитрий Жемчужнов <zhemchuzhnov.d@gmail.com>
2022-11-11 23:32:44 +0300

    реализована валидация введённого IP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIp } from "./helpers/index";
+
 const ipInput = document.querySelector(".search-bar__input");
 const searchBtn = document.querySelector(".search-bar__btn");
 
@@ -5,12 +7,13 @@ searchBtn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
 function getData() {
-  // проверка данных
-  fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_d2vFe44cbD3NieidhRwkHatHSwSg4&ipAddress=${ipInput.value}`
-  )
-    .then((response) => response.json())
-    .then(console.log);
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_d2vFe44cbD3NieidhRwkHatHSwSg4&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
