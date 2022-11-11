763cd7be287b888712b8f204c97700447461d2db
detistalingrada <zhemchuzhnov.d@gmail.com>
2022-11-11 16:51:21 +0300

    реализован функционал получения данных по IP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const ipInput = document.querySelector(".search-bar__input");
+const searchBtn = document.querySelector(".search-bar__btn");
+
+searchBtn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  // проверка данных
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_d2vFe44cbD3NieidhRwkHatHSwSg4&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
