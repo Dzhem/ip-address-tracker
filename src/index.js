9102caf4fea8c87c492924e1ba08930d90d1674c
Дмитрий Жемчужнов <zhemchuzhnov.d@gmail.com>
2022-11-13 10:21:05 +0300

    Реализована отрисовка полученных данных в DOM в блок вывода

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { validateIp } from "./helpers/index";
 
 const ipInput = document.querySelector(".search-bar__input");
 const searchBtn = document.querySelector(".search-bar__btn");
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
 
 searchBtn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
@@ -12,8 +16,16 @@ function getData() {
       `https://geo.ipify.org/api/v2/country,city?apiKey=at_d2vFe44cbD3NieidhRwkHatHSwSg4&ipAddress=${ipInput.value}`
     )
       .then((response) => response.json())
-      .then(console.log);
+      .then(printData);
   }
+}
+
+function printData(locationObj) {
+  ipInfo.innerText = locationObj.ip;
+  locationInfo.innerText =
+    locationObj.location.city + ", " + locationObj.location.region;
+  timezoneInfo.innerText = locationObj.location.timezone;
+  ispInfo.innerText = locationObj.isp;
 }
 
 function handleKey(e) {
